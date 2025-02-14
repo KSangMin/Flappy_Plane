5c1a4dc735e7a76f40936db1bbcfff7c479118b5
KSangMin <fhfpszmfnwm@naver.com>
2025-02-14 15:22:03 +0900

    [Chore] 밸런스 조정

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float maxY = 1f;
-    public float minY = -1f;
+    public float maxY = 1.2f;
+    public float minY = -1.2f;
 
-    public float minSize = 1f;
-    public float maxSize = 3f; 
+    float minSize = 0.5f;
+    float maxSize = 4f; 
 
     public Transform top;
     public Transform bottom;
@@ -17,13 +17,11 @@ public class Obstacle : MonoBehaviour
 
     public Vector2 SetRandomPlace(Vector2 lastPos, int count)
     {
-        float size = Random.Range(minSize, maxSize);
-        float halfSize = size / 2;
-
-        top.localPosition = new Vector2(0, halfSize);
-        bottom.localPosition = new Vector2(0, -halfSize);
+        top.localPosition = new Vector2(0, Random.Range(minSize, maxSize) / 2);
+        bottom.localPosition = new Vector2(0, -Random.Range(minSize, maxSize) / 2);
 
         Vector2 pos = lastPos + new Vector2(widthPadding, 0);
+        pos.y = Random.Range(minY, maxY);
 
         transform.position = pos;
 
